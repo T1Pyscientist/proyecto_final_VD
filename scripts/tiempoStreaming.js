3d1442617bf31b277ab99dc2f966618bc265ed0f
Juan Silvestri <silvestrijuanignacio@gmail.com>
2023-07-04 11:37:37 -0300

    cambio botones, seleccion auto

--- scripts/tiempoStreaming.js
+++ scripts/tiempoStreaming.js
@@ -100,18 +100,39 @@
 
 //-------------//
 
-function toggleGraphAndText(file, divId, name, totalmin) {
+window.addEventListener('DOMContentLoaded', async function () {
+    toggleGraphAndText('resources/timeStreamMonth.csv', 'graph', 'Juan');
+});
+
+
+async function toggleGraphAndText(file, divId, name) {
     const graphDiv = document.getElementById(divId);
     const dynamicText = document.getElementById('dynamicText');
     const streamingText = document.getElementById('streamingText');
+    const b1 = document.getElementsByClassName('graph-button1')[0];
+    const b2 = document.getElementsByClassName('graph-button2')[0];
+
+    // Cambiar color boton seleccionado
+    if (name == 'Juan') {
+        b1.style.backgroundColor = '#B9F5C1';
+        b2.style.backgroundColor = '#ffffff';
+    } else {
+        b2.style.backgroundColor = '#f8907e91';
+        b1.style.backgroundColor = '#ffffff';
+    }
+
 
     // Si el gráfico ya existe, lo eliminamos y salimos de la función
     if (graphDiv.hasChildNodes()) {
         graphDiv.innerHTML = '';
         dynamicText.innerHTML = '';
         streamingText.style.display = 'none';
-        return;
+        // return;
     }
+
+    // Calcular total de minutos
+    const streamingData = await d3.csv(file);
+    const totalmin = Math.floor(streamingData.reduce((acc, cur) => acc + (+cur.Streams), 0) / 60000);
 
     // Si el gráfico no existe, lo creamos
     showGraphAndText(file, divId, name, totalmin);
@@ -119,7 +140,7 @@ function toggleGraphAndText(file, divId, name, totalmin) {
 
 function showGraphAndText(file, divId, name, totalmin) {
     // Aquí reemplazas [insertar número de horas] y [insertar número de días] por los valores que correspondan
-    const text = `Comenzando con una sorpresa reveladora, el tiempo escuchado en Spotify durante este año fue de ${totalmin}. Esto es equivalente a [insertar número de días] días de música. Sin embargo, varía mucho de un mes a otro, como se muestra en la gráfica de ${name} a continuación.`;
+    const text = `Comenzando con una sorpresa reveladora, el tiempo escuchado en Spotify durante este año fue de ${totalmin} minutos. Esto es equivalente a ${(totalmin/(60*24)).toFixed(2)} días de música. Sin embargo, varía mucho de un mes a otro, como se muestra en la gráfica de ${name} a continuación.`;
 
     document.getElementById('dynamicText').innerHTML = text;
 
